c5625efcc488027afdeb22864fd5b2c9b87385a3
Erin <westonerin1@gmail.com>
2021-05-24 19:54:07 -0400

    add solution #5

--- conditionals_ruby.rb
+++ conditionals_ruby.rb
@@ -39,6 +39,19 @@ else
 end
 
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+number = 28
+if number < 10
+  p 9
+elsif number < 20
+  p 19
+elsif number < 30
+  p 29
+else
+  p -1
+end
+
+
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
