ddd6dbac959c88c102379634a356bf5de02eff9b
Erin <westonerin1@gmail.com>
2021-05-24 19:42:59 -0400

    answer #2

--- conditionals_ruby.rb
+++ conditionals_ruby.rb
@@ -9,6 +9,16 @@ end
 
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+number = 12
+if number < 10
+  p -1
+elsif number > 10
+  p 1
+else
+  p 0
+end
+
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
