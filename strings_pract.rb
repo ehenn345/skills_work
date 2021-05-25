7d1b3b026d8fe04edf4d4191d2164735d95bd874
Erin <westonerin1@gmail.com>
2021-05-24 20:23:34 -0400

    solution 4

--- strings_pract.rb
+++ strings_pract.rb
@@ -19,6 +19,10 @@ if word == "marco"
 end
 
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+color1 = "red"
+color2 = "orange"
+color3 = "blue"
+p color1 + " " + color2 + " " + color3 + " is the sentence!"
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
