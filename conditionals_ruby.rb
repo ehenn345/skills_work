7508db7827fce2e0d2c403380e606963a1760fb9
Erin <westonerin1@gmail.com>
2021-05-24 20:00:46 -0400

    solution #6

--- conditionals_ruby.rb
+++ conditionals_ruby.rb
@@ -53,6 +53,15 @@ end
 
 
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+num1 = 5
+num2 = 11
+if num1 > 10 || num2 > 10
+  p 100
+else
+  p -100
+end
+
 # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
