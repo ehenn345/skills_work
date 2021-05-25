ba3cb078bbc9db89e9aea42f668166c06f9de2ec
Erin <westonerin1@gmail.com>
2021-05-24 20:17:08 -0400

    solution #1

--- strings_pract.rb
+++ strings_pract.rb
@@ -1,4 +1,7 @@
 # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+first_name = "jen"
+last_name = "armstrong"
+p first_name + " " + last_name
 
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
